f18c50b93b419dca7beb89200af9826764d736fa
itterum <ivan.lyashenko.it@gmail.com>
2026-08-26 14:46:25 +0300

    add custom cursor pointer

--- home/itterum/default.nix
+++ home/itterum/default.nix
@@ -1,9 +1,19 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   home.username = "itterum";
   home.homeDirectory = "/home/itterum";
   home.stateVersion = "26.05";
+
+  home.pointerCursor = {
+    gtk.enable = true;       
+    x11.enable = true;       
+    hyprcursor.enable = true; 
+    
+    package = pkgs.catppuccin-cursors; 
+    name = "catppuccin-mocha-dark-cursors"; 
+    size = 16;
+  };
 
   programs.home-manager.enable = true;
 }
